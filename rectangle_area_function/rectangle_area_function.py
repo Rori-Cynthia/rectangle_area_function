2ea13d83e7a031c02d2da07f29450befe88da651
Cynthia Rojas Rivera <rori.cynthia@gmail.com>
2024-11-04 03:20:42 -0300

    Agrega validación de numero entero positivo

--- rectangle_area_function/rectangle_area_function.py
+++ rectangle_area_function/rectangle_area_function.py
@@ -7,10 +7,13 @@ def run():
         input_base = int(input("Base del rectangulo: "))
         input_height = int(input("Altura del rectangulo: "))
 
+        if input_base <= 0 or input_height <= 0:
+            print("Ha ingresado un valor igual o menor a 0 como base o altura. Intente nuevamente")
+            run()
         result = calculate_rectangle_area(input_base, input_height)
         print(f"El area del rectangulo de base {input_base} y altura {input_height} es {result}")
     except ValueError:
-        print("Entrada invalida, porfavor, ingrese numeros enteros positivos.")
+        print("Entrada invalida, porfavor, solo ingrese numeros enteros positivos.")
         run()
 
 
